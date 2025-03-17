f5d020d3cbe00bc55a4ccacdd8d9ab5d241fcf21
Shivani <shivanipalya55@gmail.com>
2025-03-17 23:45:51 +0530

    feat: Add Prisma setup with PostgreSql schema and migrations

---
+++ src/lib/prisma.ts
@@ -0,0 +1,16 @@
+// import { PrismaClient } from "@prisma/client/extension";
+
+// const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+// const prisma = globalForPrisma.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// export default prisma;
+
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default prisma;
